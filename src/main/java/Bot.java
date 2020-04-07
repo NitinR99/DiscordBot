8773aec02266684189392537e5b1a75bf924c84b
NitinR99 <rames11a@uwindsor.ca>
2020-04-07 03:49:28 -0700

    update gitignore for key

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,11 +2,16 @@ import events.Brain;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Bot
 {
-    public static void main(String args[]) throws Exception{
-
-        JDA jda = new JDABuilder("Njk2OTUyNzgwNDgwNTc3NTg4.XowOCg.X-6hYrvySXMGFVAjzodz9g0nqUY").build();
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader (new FileReader("keys.txt") ) ;
+        String mykey=br.readLine().strip();
+        JDA jda = new JDABuilder(mykey).build();
         jda.addEventListener(new Brain());
     }
 }
